df1564d7dd368afbce3c638e5be6a193d3d532e1
Michael Catanzaro <mcatanzaro@gnome.org>
2015-09-05 09:19:18 -0500

    All Iulian's work as one commit
    We don't want to merge this into master because we want to keep your
    history. But GitHub makes it basically impossible to review an entire
    branch of changes at once. So pushing this to a sidebranch as one commit
    just makes it easy to leave review comments. You'll want to make changes
    on your main branch, as usual.

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,36 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from GNOME git (or from"
-    echo "your OS vendor's package manager)."
-    exit 1
+(test -f $srcdir/configure.ac) || {
+        echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level project directory"
+        exit 1
 }
 
-REQUIRED_AUTOMAKE_VERSION=1.9 
-REQUIRED_YELP_TOOLS_VERSION=3.1.1
-REQUIRED_GETTEXT_VERSION=0.12
-REQUIRED_INTLTOOL_VERSION=0.40.4
+PKG_NAME=`autoconf --trace 'AC_INIT:$1' "$srcdir/configure.ac"`
 
-. gnome-autogen.sh
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+        echo "**Warning**: I am going to run \`configure' with no arguments." >&2
+        echo "If you wish to pass any to it, please specify them on the" >&2
+        echo \`$0\'" command line." >&2
+        echo "" >&2
+fi
+
+set -x
+aclocal --install || exit 1
+intltoolize --force --copy --automake || exit 1
+autoreconf --verbose --force --install -Wno-portability || exit 1
+{ set +x; } 2>/dev/null
+
+if [ "$NOCONFIGURE" = "" ]; then
+        set -x
+        $srcdir/configure "$@" || exit 1
+        { set +x; } 2>/dev/null
+
+        if [ "$1" = "--help" ]; then exit 0 else
+                echo "Now type \`make\' to compile $PKG_NAME" || exit 1
+        fi
+else
+        echo "Skipping configure process."
+fi
